c131165b3151f26c521bf5282722a2351e09a50a
Santo <santonurul1@gmail.com>
2025-04-07 23:53:25 +0600

    code-add

---
+++ bin/beginner_level/data_type/categories.dart
@@ -0,0 +1,35 @@
+
+void main () {
+
+
+  // Primitive data type
+
+  /**
+   *
+   * string,int,double,bool
+   *
+   */
+
+  // Collections data type
+
+  /**
+   * List,map,set
+   */
+
+  // Dynamic and Var Types
+
+  /**
+   * var,dynamic
+   */
+
+  // Null Safety type
+
+  /**
+   * Non-nullable, Nullable,
+   */
+
+  //  Custom Data Types
+  /**
+   * enum,class
+   */
+}
