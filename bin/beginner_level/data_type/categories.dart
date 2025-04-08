86618f06bfa3abfa1b77154a898c2b21f88a6b7d
Santo <santonurul1@gmail.com>
2025-04-08 23:34:48 +0600

    code-add

--- bin/beginner_level/data_type/categories.dart
+++ bin/beginner_level/data_type/categories.dart
@@ -32,4 +32,21 @@ void main () {
   /**
    * enum,class
    */
+
+
+  // Data type example
+  /**
+      int example >>>  int a = 10;
+      double	example >>> 	double pi = 3.14;
+      num	example >>>  (int বা double)	num x = 10;
+      bool	example >>> 	bool isOk = true;
+      String	example >>> 	String name = 'Ali';
+      List	example >>> 	List<int> nums = [1,2,3];
+      Set	example >>> 	Set<String> tags = {'a','b'}
+      Map	example >>> 	Map<String, int> ages = {'Ali': 30}
+      dynamic	example >>> 	dynamic data = 'hi';
+      var	example >>> 	var a = 5;
+      enum	example >>> 	enum Color { red, blue }
+      Object	example >>> 	Object x = 'text';
+   */
 }
---
+++ bin/beginner_level/data_type/primitive_data_type/string_data_type.dart
@@ -0,0 +1,92 @@
+
+void main () {
+
+  // Single line String declare
+  String singleLineString = 'Santo';
+  print('Single Line string : $singleLineString'); // would be the correct answer =>  Single Line string : Santo
+
+  // Multi line String declare
+  String multiLineString = '''
+  Dart, a versatile, open-source programming language developed by Google, 
+  is designed for building applications across various platforms, including web,
+   mobile, server, and desktop, and is particularly known for its use with the Flutter framework
+   ''';
+  print('MultiLine string : $multiLineString');
+
+  // String Interpolation
+  String interpolation1 = 'Md';
+  String interpolation2 = 'Santo';
+  print( 'String interpolation name : $interpolation1 $interpolation2' ); // would be the correct answer => String interpolation name : Md Santo
+
+  // String Concatenation
+  String firstName = 'Md';
+  String lastName = 'Santo';
+  print( firstName + ' ' + lastName); // would be the correct answer => Md Santo
+
+
+  // String operation or Common String Methods or formating
+  String operation = 'operation';
+  print(operation.length);
+  print(operation.toLowerCase()); // would be the correct answer => operation
+  print(operation.toUpperCase()); // would be the correct answer => OPERATION
+  print(operation.substring(0,1)); //  would be the correct answer =>  op
+  print(operation.contains('op')); // would be the correct answer => true
+  print(operation.startsWith('o')); // would be the correct answer => true
+  print(operation.endsWith('n')); // would be the correct answer => true
+  print(operation.trim()); // would be the correct answer => operation
+  print(operation.replaceAll('o', 'O')); // would be the correct answer => Operation
+
+
+  // String empty check
+  String empty = '';
+  print(empty.isEmpty); // would be the correct answer => true
+  print(empty.isNotEmpty); // would be the correct answer => false
+
+  // String to character access
+  String name = 'Md santo';
+  print(name[0]); // would be the correct answer => M
+  print(name[name.length - 1]); // would be the correct answer => t
+
+
+  // Raw String ,New line, tab
+  String normalString = 'Iam software developer \n My name is santo'; // New line crate hoi
+  String space = 'Iam \tsoftware developer'; // Four character space crate hoi
+  String rawString = r'Iam software developer \n My name is santo';
+  print(normalString);
+  print(rawString);
+  print(space);
+
+
+  // String to int / double convert
+  String roll = '100';
+  int intRoll = int.parse(roll); // Would be correct answer => 100
+  double doubleRoll = double.parse(roll); // Would be correct answer => 100.0
+  print(intRoll);
+  print(doubleRoll);
+
+  // Int or Double to string convert
+  int age = 19;
+  double doubleAge = 19.0;
+  String stringAge = age.toString();
+  String ageString = doubleAge.toString();
+  print(stringAge);
+  print(ageString);
+
+  // Null Safety  String?
+   String? nullString ;
+   String noNull = 'No null string' ;
+   print(nullString); // Would be correct answer => null
+  print(noNull); // Would be correct answer => null
+
+
+  // String Buffer (Performance Optimization)
+  StringBuffer buffer = StringBuffer();
+  buffer.write('Md\t');
+  buffer.write('Sakibul hasan');
+  buffer.write('\tsanto');
+  print(buffer.toString());
+
+
+
+
+}
